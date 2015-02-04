3a12c2f8e96a1e9773f4aa722aab3cec0dd97ce5
Dominic May <jack.thatch@gmail.com>
2015-02-04 23:19:38 +0800

    Move binary converstion utils inside lib

--- circuitry/util.py
+++ circuitry/util.py
@@ -16,6 +16,19 @@ def get_pos(pos, instring):
     return Pos(line, column)
 
 
+def as_bin(i):
+    return tuple(map(int, bin(i)[2:].rjust(8, '0')))
+
+
+def from_bin(i):
+    if len(i) == 9:
+        sign, *i = i
+
+    i = int(''.join(map(str, i)), 2)
+
+    return -i if sign else i
+
+
 class StateDict(UserDict):
     def __setitem__(self, name, val):
         assert val in {0, 1}
--- tests/utils.py
+++ tests/utils.py
@@ -27,19 +27,6 @@ def get_custom_component(filename, ttype):
     return get_graph(filename).get(ttype)
 
 
-def as_bin(i):
-    return tuple(map(int, bin(i)[2:].rjust(8, '0')))
-
-
-def from_bin(i):
-    if len(i) == 9:
-        sign, *i = i
-
-    i = int(''.join(map(str, i)), 2)
-
-    return -i if sign else i
-
-
 def build_pins(num, binary):
     pins = ['{}{}'.format(l, num) for l in ALPHA_8BIT]
 
